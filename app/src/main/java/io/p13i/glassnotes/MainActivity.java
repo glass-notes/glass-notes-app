92d3ff69b5e524a62cc1346c5ab0b6172c25c37b
Pramod Kotipalli <pramod.kotipalli@gmail.com>
2019-07-05 21:13:03 -0700

    App launches!

--- app/src/main/java/io/p13i/glassnotes/MainActivity.java
+++ app/src/main/java/io/p13i/glassnotes/MainActivity.java
@@ -2,9 +2,10 @@ package io.p13i.glassnotes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
